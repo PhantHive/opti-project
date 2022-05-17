fcaaf8c048edcbedf375053d74d3b8792aed2057
coren <corentin.borg@ipsa.Fr>
2022-05-17 21:30:11 +0200

    update 1 17/05/2022 : modification des fenetres, fondation de l'ajout des fenetre de changement de equation et de methode

--- src/pages/chang_method.py
+++ src/pages/chang_method.py
@@ -8,13 +8,72 @@ from PyQt5.QtWidgets import QPushButton, QLabel, QWidget, QLineEdit, QComboBox, 
 
 from src.canvas.Canvas import Canvas
 
+class EquationWin(object):
+    def __init__(self): 
+        self.screen = QtWidgets.QDesktopWidget().screenGeometry()
+        self.width = int(self.screen.width() * 0.50)
+        self.height = int(self.screen.height() * 0.55)
 
-class StartWin(object):
+        self.onlyInt = QIntValidator()
+        self.lang = None
+    
+    def setupUI(self,EquationWin):
+        EquationWin.setGeometry((self.screen.width() - self.width) // 2, (self.screen.height() - self.height) // 2,
+                             self.width, self.height)
+        EquationWin.setFixedSize(self.width, self.height)
 
-    def __init__(self):
-        '''
-        choose the equation to solve within a specific interval
-        '''
+        EquationWin.setWindowTitle(self.lang["app-title"] + " \ " + self.lang["unk"])
+
+        self.start_widgets = QWidget(EquationWin)
+
+
+        self.language = QPushButton(self.start_widgets)
+        self.language.setText(self.lang["language"])
+
+        self.entry_widgets()
+        self.result_widgets()
+        self.move_widgets()
+
+        EquationWin.setCentralWidget(self.start_widgets)
+        
+    def entry_widgets(self): 
+        self.home_bt = QPushButton(self.start_widgets)
+        self.home_bt.setText(self.lang["home"])
+
+        self.home_bt.resize(130, 55)
+        self.home_bt.setProperty("type", 1)
+
+        self.equa1 = QPushButton(self.change_equation)  # change to equation 1
+        self.equa1.resize(250, 45)
+        self.equa1.setText(self.lang["unk"])
+        self.equa1.setProperty("type", 2)
+        
+        self.equa2 = QPushButton(self.change_equation)  # change to equation 2
+        self.equa2.resize(250, 45)
+        self.equa2.setText(self.lang["unk"])
+        self.equa2.setProperty("type", 2)
+        
+        self.equa3 = QPushButton(self.change_equation)  # change to equation 3 
+        self.equa3.resize(250, 45)
+        self.equa3.setText(self.lang["unk"])
+        self.equa3.setProperty("type", 2)
+        
+        self.equa4 = QPushButton(self.change_equation)  # change to equation 4 
+        self.equa4.resize(250, 45)
+        self.equa4.setText(self.lang["unk"])
+        self.equa4.setProperty("type", 2)
+        
+        self.equa5 = QPushButton(self.change_equation)  # change to equation 5
+        self.equa5.resize(250, 45)
+        self.equa5.setText(self.lang["unk"])
+        self.equa5.setProperty("type", 2)
+    def return_start(self):
+        pass
+    def change_equation(self, a ):
+        print(a)
+    
+    """
+
 
         self.screen = QtWidgets.QDesktopWidget().screenGeometry()
         self.width = int(self.screen.width() * 0.50)
@@ -36,7 +95,7 @@ class StartWin(object):
 
         StartWin.setWindowTitle(self.lang["app-title"] + " \ " + self.lang["start"])
 
-        self.start_widgets = QWidget(StartWin)
+        self.change_equation = QWidget(StartWin)
 
 
         self.language = QPushButton(self.start_widgets)
@@ -88,8 +147,6 @@ class StartWin(object):
         self.intervaly.setGraphicsEffect(shadow)
         self.change_eq.setGraphicsEffect(shadow)
         self.method.setGraphicsEffect(shadow)
-    def start_change_equ(self):
-        pass
 
     def result_widgets(self):
         # Result
@@ -99,12 +156,10 @@ class StartWin(object):
 
         self.language.move(int(self.width * 0.95), int(self.height * 0.015))
         self.change_eq.move(int(self.width * 0.1), int(self.height * 0.15))
-        self.intervalx.move(int(self.width * 0.1), int(self.height * 0.5))
-        self.intervaly.move(int(self.width * 0.1), int(self.height * 0.6))
-        self.equation.move(int(self.width * 0.1), int(self.height * 0.30))
+        self.intervalx.move(int(self.width * 0.5), int(self.height * 0.35))
+        self.intervaly.move(int(self.width * 0.5), int(self.height * 0.45))
+        self.equation.move(int(self.width * 0.1), int(self.height * 0.55))
         self.method.move(int(self.width * 0.5), int(self.height * 0.75))
         self.home_bt.move(int(self.width * 0.85), int(self.height * 0.9))
 
-    def calculate(self):
-        pass
-
+"""
